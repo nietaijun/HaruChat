14717c143e5fd6020c8260a698f1e21a95244e9b
savior@QvQ <2990790038@qq.com>
2025-12-06 23:23:02 +0800

    Initial Commit

---
+++ HaruChat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HaruChat
+//
+//  Created by 玩忽所以 on 2025/12/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HaruChat/HaruChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaruChatApp.swift
+//  HaruChat
+//
+//  Created by 玩忽所以 on 2025/12/6.
+//
+
+import SwiftUI
+
+@main
+struct HaruChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
